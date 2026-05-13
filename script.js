6497d3334d5a0601bbcf90f41c0cb2eb004a04d7
AbdielOlivares <abdiel.olivarabi@gmail.com>
2026-05-13 14:33:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const burger = document.queryselector('.burger');
+
+const nav = document.queryselector('.nav-links');
+
+burger. addeventlistener ('click',()=>{
+nav.style.display=nav.style.display==='flex'
+?'none':'flex';
+});
