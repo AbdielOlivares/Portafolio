2d1a68c4fc9d67e2639ab4299560174b163ce5d6
AbdielOlivares <abdiel.olivarabi@gmail.com>
2026-05-13 14:50:21 -0400

    Delete Coding directory

--- Coding/script.js
+++
@@ -1,7 +0,0 @@
-const burger = document.getElementById("burger");
-const navMenu = document.getElementById("nav-menu");
-
-burger.addEventListener("click", () => {
-	navMenu.style.display =
-		navMenu.style.display === "flex" ? "none" : "flex";
-});
