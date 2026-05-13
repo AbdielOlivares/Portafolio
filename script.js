bc08389aae9a1ce1d5942467bcd20f5c152ac9e5
AbdielOlivares <abdiel.olivarabi@gmail.com>
2026-05-13 14:47:05 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const burger = document.queryselector('.burger');
-
-const nav = document.queryselector('.nav-links');
-
-burger. addeventlistener ('click',()=>{
-nav.style.display=nav.style.display==='flex'
-?'none':'flex';
-});
